b98bfc257d1494abb458b86c0c405026568dd4d5
Andrew Whatson <whatson@gmail.com>
2020-12-06 23:12:52 +1000

    Solution for Day 4 Part 2

--- day-4-passport-processing.scm
+++ day-4-passport-processing.scm
@@ -24,18 +24,58 @@
                (lines (cdr lines)))
           (loop lines record records)))))
 
-(define (process-passports-1 lines)
+(define (process-passports-1 passports)
   (count (lambda (passport)
            (every (lambda (field)
-                    (assoc field passport string=?))
+                    (assoc field passport))
                   '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid")))
-         (parse-passports lines)))
+         passports))
 
-(define (process-passports-2 lines)
-  #t)
+(define (verify key check passport)
+  (check (cdr (assoc key passport))))
+
+(define (check-birth-year value)
+  (let ((byr (string->number value)))
+    (and (>= byr 1920) (<= byr 2002))))
+
+(define (check-issue-year value)
+  (let ((iyr (string->number value)))
+    (and (>= iyr 2010) (<= iyr 2020))))
+
+(define (check-expiration-year value)
+  (let ((eyr (string->number value)))
+    (and (>= eyr 2020) (<= eyr 2030))))
+
+(define (check-height value)
+  (let* ((m (regexp-matches '(: ($ (+ num)) ($ (or "cm" "in"))) value))
+         (hgt (string->number (regexp-match-submatch m 1)))
+         (unit (regexp-match-submatch m 2)))
+    (or (and (string=? unit "cm") (>= hgt 150) (<= hgt 193))
+        (and (string=? unit "in") (>= hgt  59) (<= hgt  76)))))
+
+(define (check-hair-color value)
+  (regexp-matches? '(: #\# (= 6 hex-digit)) value))
+
+(define (check-eye-color value)
+  (regexp-matches? '(or "amb" "blu" "brn" "gry" "grn" "hzl" "oth") value))
+
+(define (check-passport-id value)
+  (regexp-matches? '(= 9 numeric) value))
+
+(define (process-passports-2 passports)
+  (count (lambda (p)
+           (guard (error (else #f))
+             (and (verify "byr" check-birth-year p)
+                  (verify "iyr" check-issue-year p)
+                  (verify "eyr" check-expiration-year p)
+                  (verify "hgt" check-height p)
+                  (verify "hcl" check-hair-color p)
+                  (verify "ecl" check-eye-color p)
+                  (verify "pid" check-passport-id p))))
+         passports))
 
 (run-advent-program
  process-passports-1
  process-passports-2
  (lambda (file process)
-   (process (file->lines file))))
+   (process (parse-passports (file->lines file)))))
