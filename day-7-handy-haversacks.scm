230bb14dc01adab4ac7cbb4abe4a455793ad3935
Andrew Whatson <whatson@gmail.com>
2020-12-07 22:38:14 +1000

    Solution for Day 7 Part 1

---
+++ day-7-handy-haversacks.scm
@@ -0,0 +1,68 @@
+#!/usr/bin/env -S guile --r7rs -s
+!# ; -*- mode: scheme -*-
+(import (scheme base)
+        (scheme comparator)
+        (scheme list)
+        (scheme hash-table)
+        (scheme regex)
+        (advent-utils))
+
+(define string-comparator
+  (make-comparator string? string=? string<? string-hash))
+
+(define (parse-inner-bags str)
+  (if (string=? str "no other bags.")
+      '()
+      (let loop ((i 0) (bags '()))
+        (let ((m (regexp-search '(: bos
+                                    ($ (+ numeric)) " "
+                                    ($ (+ alpha) " " (+ alpha))
+                                    " bag" (? "s") ($ (or ", " ".")))
+                                str i)))
+          (if (not m)
+              (error "Invalid rule" str)
+              (let* ((bag-count (string->number (regexp-match-submatch m 1)))
+                     (bag-type (regexp-match-submatch m 2))
+                     (end-str (regexp-match-submatch m 3))
+                     (end-pos (regexp-match-submatch-end m 3))
+                     (bag (cons bag-type bag-count))
+                     (bags (cons bag bags)))
+                (if (string=? end-str ".")
+                    bags
+                    (loop end-pos bags))))))))
+
+(define (parse-rule line)
+  (let* ((parts (regexp-split " bags contain " line))
+         (outer-bag (first parts))
+         (inner-bags (parse-inner-bags (second parts))))
+    (values outer-bag inner-bags)))
+
+(define (contains? rules cache bag want)
+  (or (hash-table-ref/default cache bag #f)
+      (let ((result (or (string=? bag want)
+                        (any (lambda (p)
+                               (contains? rules cache (car p) want))
+                             (hash-table-ref/default rules bag '())))))
+        (hash-table-set! cache bag result)
+        result)))
+
+(define (process-rules-1 lines)
+  (let ((rules (make-hash-table string-comparator))
+        (cache (make-hash-table string-comparator)))
+    (for-each (lambda (line)
+                (let-values (((outer-bag inner-bags) (parse-rule line)))
+                  (hash-table-set! rules outer-bag inner-bags)))
+              lines)
+    (count (lambda (bag)
+             (and (not (string=? bag "shiny gold"))
+                  (contains? rules cache bag "shiny gold")))
+           (hash-table-keys rules))))
+
+(define (process-rules-2 lines)
+  #t)
+
+(run-advent-program
+ process-rules-1
+ process-rules-2
+ (lambda (process)
+   (process (file->lines "data/day-7.txt"))))
